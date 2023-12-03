c3cbe00ce5ad908a7c9b6ed8987aa6941cc3b0bf
JonettaPek <jpek009@e.ntu.edu.sg>
2023-12-03 17:25:29 +0800

    UITabBarController

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -31,8 +31,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        window?.rootViewController = OnboardingViewController(withImage: "delorean", withText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in the 80s.")
 //        window?.rootViewController = loginViewController
         
-        let navigationController = UINavigationController(rootViewController: ViewController())
-        window?.rootViewController = navigationController
+//        let navigationController = UINavigationController(rootViewController: ViewController())
+//        window?.rootViewController = navigationController
+        
+        let vc1 = SearchViewController()
+        let vc2 = ContactsViewController()
+        let vc3 = FavoritesViewController()
+        
+        vc1.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        vc2.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 1)
+        vc3.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 2)
+        
+        let nc1 = UINavigationController(rootViewController: vc1)
+        let nc2 = UINavigationController(rootViewController: vc2)
+        let nc3 = UINavigationController(rootViewController: vc3)
+        
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [nc1, nc2, nc3]
+        
+        window?.rootViewController = tabBarController
         
         return true
     }
---
+++ Bankey/Bankey/Containers/TabBarControllers.swift
@@ -0,0 +1,26 @@
+//
+//  TabBarControllers.swift
+//  Bankey
+//
+//  Created by Jonetta Pek on 3/12/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    override func viewDidLoad() {
+        title = "Search"
+    }
+}
+
+class ContactsViewController: UIViewController {
+    override func viewDidLoad() {
+        title = "Contacts"
+    }
+}
+
+class FavoritesViewController: UIViewController {
+    override func viewDidLoad() {
+        title = "Favorites"
+    }
+}
